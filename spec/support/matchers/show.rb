8b45187e8b2e4027c0540b82e0f4830af3392e9b
Amanda Dolan <amandacrawfordalc@gmail.com>
2017-08-02 12:01:53 -0400

    Adds details to the failure message for the `show` matcher

--- spec/support/matchers/show.rb
+++ spec/support/matchers/show.rb
@@ -3,11 +3,34 @@ RSpec::Matchers.define :show do |page_object_name|
     page_object.on_page? && page_object.visible?
   end
 
+  failure_message do |_page|
+    <<~TEXT
+      The page object "#{page_object_name}" wasn't on the page when the object
+      was accessed.
+        - The current page title is "#{title}".
+        - Classes applied to the main element are "#{main_classes}".
+    TEXT
+  end
+
   match_when_negated do |_page|
     !page_object.on_page? || page_object.hidden?
   end
 
   define_method :page_object do
     send(page_object_name, true)
+  end
+
+  define_method :title do
+    page.title
+  end
+
+  define_method :main_classes do
+    main_element = page.find('main')
+    if main_element.present?
+      classes = main_element[:class]
+      classes.present? ? classes : '(no classes applied to main element)'
+    else
+      '(no main element found)'
+    end
   end
 end
