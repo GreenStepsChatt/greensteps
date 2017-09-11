3a95ca36202bd1576e4a003a19f024e6cc38c156
Amanda Dolan <amandacrawfordalc@gmail.com>
2017-09-11 09:59:58 -0400

    Use page object and corresponding matchers rather than looking at URL

--- spec/features/guest_can_not_see_dashboard_spec.rb
+++ spec/features/guest_can_not_see_dashboard_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature 'Dashboard', type: :feature do
   scenario 'Guests are asked to log in first' do
     visit dashboards_show_path
 
-    expect(current_path).to eq(root_path)
+    expect(page).to show :welcome_page
     expect(page).to flash_message t('not_logged_in')
   end
 end
