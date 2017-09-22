183dc2d080f67606145477a5fc0008beaff95453
Amanda Dolan <amandacrawfordalc@gmail.com>
2017-09-22 12:47:34 -0400

    Fix spacing from two commits ago

--- app/models/deed.rb
+++ app/models/deed.rb
@@ -1,7 +1,7 @@
 class Deed < ApplicationRecord
   belongs_to :user
   validate :something_was_done,
-    if: ->(deed) { deed.miles.present? && deed.trash_bags.present? }
+           if: ->(deed) { deed.miles.present? && deed.trash_bags.present? }
   validates :miles, :trash_bags, presence: true
 
   def something_was_done
