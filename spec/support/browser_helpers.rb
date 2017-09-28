6fdcc42c1b607c56a1ff6aefca75ad3c7705c741
Adam Radabaugh <adam@walkingtoast.com>
2017-09-27 17:40:04 -0700

    Fix Issue#1 by adding an explicit move to root path.

--- spec/support/browser_helpers.rb
+++
@@ -1,9 +0,0 @@
-module BrowserHelpers
-  def click_browser_back_button
-    page.evaluate_script('window.history.back()')
-  end
-end
-
-RSpec.configure do |config|
-  config.include BrowserHelpers, type: :feature
-end
