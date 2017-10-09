2b9819550a8ecc5bdd0af9c345b93f9c97bb0b6f
Sundus Yousuf <sundus.ahmed.yousuf@gmail.com>
2017-10-08 21:43:52 -0500

    PR Review Update
    * Changes the method name to a more descriptive name and makes it a
      private function since it's not to be used outside the helper module.

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -2,10 +2,12 @@ module DeviseHelper
   def devise_error_messages!
     return '' if resource.errors.empty?
     content_tag(:div, id: 'error_explanation') do
-      safe_join [content_tag(:h3, formatted_sentence),
+      safe_join [content_tag(:h3, error_header),
                  content_tag(:ul, safe_join(full_messages_in_li))]
     end
   end
+
+  private
 
   def full_messages_in_li
     resource.errors.full_messages.map do |msg|
@@ -13,7 +15,7 @@ module DeviseHelper
     end
   end
 
-  def formatted_sentence
+  def error_header
     I18n.t('errors.messages.not_saved',
            count: resource.errors.count,
         resource: resource.class.model_name.human.downcase)
