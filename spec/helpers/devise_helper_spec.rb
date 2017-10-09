b1778dea1470abb9671d9233e2ca1b860dee897a
Sundus Yousuf <sundus.ahmed.yousuf@gmail.com>
2017-10-08 23:54:45 -0500

    PR Review Update
    * Updates the rspec to actually test the functionality of the method.

--- spec/helpers/devise_helper_spec.rb
+++ spec/helpers/devise_helper_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe DeviseHelper, type: :helper do
   describe '#devise_error_messages!' do
-    it 'returns div error_messages with h3 header' do
+    before(:each) do
       without_partial_double_verification do
         allow(helper).to receive_message_chain('resource.errors')\
           .and_return(
@@ -15,9 +15,22 @@ RSpec.describe DeviseHelper, type: :helper do
         allow(helper).to \
           receive_message_chain('resource.class.model_name.human')
           .and_return('ModelName')
-        msg = '<h3>2 errors prohibited this modelname from being saved:</h3>'
-        expect(helper.devise_error_messages!).to include(msg)
       end
+    end
+
+    it 'returns h3 error message header' do
+      header = '<h3>2 errors prohibited this modelname from being saved:</h3>'
+      expect(helper.devise_error_messages!).to include(header)
+    end
+
+    it 'returns list of error messages' do
+      error_list = '<li>Error Message 1</li><li>Error Message 2</li>'
+      expect(helper.devise_error_messages!).to include(error_list)
+    end
+
+    it 'returns div with error_explanation as id' do
+      div = '<div id="error_explanation">'
+      expect(helper.devise_error_messages!).to include(div)
     end
   end
 end
