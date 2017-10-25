1ac1c0908f12b84bebb7263c009364e944bbf26a
Amanda Dolan <amandacrawfordalc@gmail.com>
2017-10-25 09:20:41 -0400

    All view specs should have `rendered` as the subject

--- spec/views/prizes/_form.html.erb_spec.rb
+++ spec/views/prizes/_form.html.erb_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'prizes/_form' do
   before(:each) { render 'prizes/form', prize: Prize.new }
-  subject { rendered }
 
   it { should require_field '#prize_title' }
   it { should require_field '#prize_quantity' }
