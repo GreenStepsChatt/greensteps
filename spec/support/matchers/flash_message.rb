20d7b649175d4803435f8af640c640367c98d231
Amanda Dolan <amandacrawfordalc@gmail.com>
2017-11-22 06:47:14 -0700

    Flash matcher should give more details in the failure message

--- spec/support/matchers/flash_message.rb
+++ spec/support/matchers/flash_message.rb
@@ -2,4 +2,24 @@ RSpec::Matchers.define :flash_message do |expected_message|
   match do |page|
     page.has_css? '.flash-message', text: expected_message
   end
+
+  failure_message do |page|
+    <<~TEXT
+      expected
+        #{flash_html}
+      to include "#{expected_message}"
+
+      more info:
+        - the page title is "#{page.title}"
+        - classes applied to the main element are "#{page.find('main')[:class]}"
+    TEXT
+  end
+
+  define_method :flash_html do
+    if page.has_css?('.flash-message')
+      page.find('.flash-message').native.to_html
+    else
+      "(no flash element present)"
+    end
+  end
 end
