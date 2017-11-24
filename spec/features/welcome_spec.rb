396f3dfd402fa949cb34c9c7e829014c41507456
Amanda Dolan <amandacrawfordalc@gmail.com>
2017-11-24 08:52:36 -0700

    Welcome spec isn't helpful now that we have functionality in the app

--- spec/features/welcome_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.feature 'Welcome', type: :feature do
-  scenario 'Visitor lands on welcome page' do
-    visit root_path
-
-    expect(page).to have_content t('welcome.index.greeting')
-  end
-end
