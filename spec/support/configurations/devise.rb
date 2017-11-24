fc668c8bfd1aa3197065d53c42ea3026b6302c93
Amanda Dolan <amandacrawfordalc@gmail.com>
2017-11-24 09:39:07 -0700

    Group all Devise controller specs in the users
    Also, set devise mapping in config file and tag devise controller specs

--- spec/support/configurations/devise.rb
+++ spec/support/configurations/devise.rb
@@ -1,3 +1,7 @@
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
+
+  config.before(:each, :devise, type: :controller) do
+    @request.env['devise.mapping'] = Devise.mappings[:user]
+  end
 end
