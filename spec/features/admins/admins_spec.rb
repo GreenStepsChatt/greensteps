244c2a273e6f0ec570ceda6afdd4bdc0d373e01c
Amanda Dolan <amandacrawfordalc@gmail.com>
2017-12-01 19:45:53 -0500

    Use page objects

--- spec/features/admins/admins_spec.rb
+++ spec/features/admins/admins_spec.rb
@@ -3,13 +3,11 @@ require 'rails_helper'
 RSpec.describe 'List of admins', type: :feature do
   scenario 'Admin logs in, clicks on link to view list of admins' do
     create_and_login_admin
-
     visit admins_dashboards_show_path
 
-    click_on t('admins.dashboards.show.miscellaneous.admins')
+    admin_dashboard.manage_admins
 
-    expect(page).to have_content t('admins.admins.index.header')
-
+    expect(page).to show :admin_list
   end
 
   scenario 'Admin grants an existing user admin privileges' do
