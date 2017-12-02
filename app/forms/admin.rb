9773da7a256ce953db97caf15103911757baaf3f
Amanda Dolan <amandacrawfordalc@gmail.com>
2017-12-01 20:34:27 -0500

    Add validations for admin form and fix `Admin#save`

--- app/forms/admin.rb
+++ app/forms/admin.rb
@@ -2,14 +2,26 @@ class Admin
   include ActiveModel::Model
 
   attr_accessor :email
+  validates_presence_of :email
+  validate :user_exists
 
   def save
-    user.add_role :admin
+    if valid?
+      user.add_role :admin
+    else
+      false
+    end
   end
 
   private
 
   def user
-    User.where(email: email).first
+    @user ||= User.where(email: email).first
+  end
+
+  def user_exists
+    unless user.present?
+      errors.add(:email, "must be an existing user's email address")
+    end
   end
 end
