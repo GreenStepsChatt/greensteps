f6bb3a81db487f07bcdc39b9afad1168e9f5ebf2
Amanda Dolan <amandacrawfordalc@gmail.com>
2017-12-06 19:59:23 -0700

    Fix RuboCop issue
    Not the best solution, but we could do something else if we need to
    respond to another format

--- app/controllers/admins/base_controller.rb
+++ app/controllers/admins/base_controller.rb
@@ -6,16 +6,14 @@ module Admins
 
     def ensure_admin
       return if current_user.has_role? :admin
+
       respond_to do |format|
         format.html do
-          redirect_back(
-            fallback_location: root_path,
-            alert: t('restricted_to_admins')
-          )
+          redirect_back fallback_location: root_path, alert: t('admins_only')
         end
 
         format.js do
-          flash.now.alert = t('restricted_to_admins')
+          flash.now.alert = t('admins_only')
           render 'shared/update_flash', status: :unauthorized
         end
       end
