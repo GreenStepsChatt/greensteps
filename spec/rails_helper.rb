7908f20d37446a422b8ce14953ab9d638c9334ae
Amanda Dolan <amandacrawfordalc@gmail.com>
2018-01-21 20:28:47 -0500

    Automatically clear logs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,6 +29,10 @@ Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.before(:suite) do
+    `rake log:clear LOG=test` if ENV.fetch('AUTO_CLEAR_LOGS')
+  end
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
