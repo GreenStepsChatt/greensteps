e05b5c716a9ab5416c84f66ee9e2ff305105cf34
Amanda Dolan <amandacrawfordalc@gmail.com>
2018-01-25 19:43:39 -0500

    Only load mail interceptor in production
    Dev and test environments don't even use a real delivery method

--- config/initializers/mail_interceptor.rb
+++ config/initializers/mail_interceptor.rb
@@ -1,4 +1,4 @@
-if ENV.fetch('INTERCEPT_EMAILS', "true") == "true"
+if Rails.env.production? && ENV.fetch('INTERCEPT_EMAILS', "true") == "true"
   interceptor = MailInterceptor::Interceptor.new(
     env: OpenStruct.new(intercept?: true),
     forward_emails_to: ENV.fetch('SEND_INTERCEPTED_EMAILS_TO')
