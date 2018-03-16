1ce666cfeca2fee4abbb72dda82d7248edcfad2c
Joe <jdg006@lvc.edu>
2018-03-16 23:10:14 +0000

    removing unintended edits to config files

--- config/puma.rb
+++ config/puma.rb
@@ -11,7 +11,6 @@ threads threads_count, threads_count
 #
 port        ENV.fetch("PORT") { 3000 }
 
-
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
