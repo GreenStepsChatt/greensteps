6a0607dd88602e1c3e8d66d99d06cc58bc8547f3
Joe <jdg006@lvc.edu>
2018-03-16 21:15:01 +0000

    formatting

--- spec/controllers/admins/prizes_controller_spec.rb
+++ spec/controllers/admins/prizes_controller_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Admins::PrizesController, type: :controller do
-  
-  let(:prize) {create(:prize)}
-  
+  let(:prize) { create(:prize) }
+
   describe 'POST #create' do
     context 'invalid prize' do
       it 're-renders the form' do
@@ -15,24 +14,23 @@ RSpec.describe Admins::PrizesController, type: :controller do
       end
     end
   end
-  
-  describe "GET edit" do
-    
-    it "returns http success" do
+
+  describe 'GET edit' do
+    it 'returns http success' do
       sign_in create(:admin)
-      get :edit, params: {id: prize.id}
+      get :edit, params: { id: prize.id }
       expect(response).to have_http_status(:success)
     end
 
-    it "renders the #edit view" do
+    it 'renders the #edit view' do
       sign_in create(:admin)
-      get :edit, params: {id: prize.id}
+      get :edit, params: { id: prize.id }
       expect(response).to render_template :edit
     end
 
-    it "assigns prize to be updated to @prize" do
+    it 'assigns prize to be updated to @prize' do
       sign_in create(:admin)
-      get :edit, params: {id: prize.id}
+      get :edit, params: { id: prize.id }
       prize_instance = assigns(:prize)
 
       expect(prize_instance.id).to eq prize.id
@@ -41,16 +39,16 @@ RSpec.describe Admins::PrizesController, type: :controller do
       expect(prize_instance.cost).to eq prize.cost
     end
   end
-  
-  describe "PUT update" do
-    it "updates prize with expected attributes" do
+
+  describe 'PUT update' do
+    it 'updates prize with expected attributes' do
       sign_in create(:admin)
       new_prize = create(:prize)
       new_title = new_prize.title
       new_quantity = new_prize.quantity
       new_cost = new_prize.cost
 
-      put :update, params: {id: prize.id, prize: {title: new_prize.title, quantity: new_prize.quantity, cost: new_prize.cost}}
+      put :update, params: { id: prize.id, prize: { title: new_prize.title, quantity: new_prize.quantity, cost: new_prize.cost } }
 
       updated_prize = assigns(:prize)
       expect(updated_prize.id).to eq prize.id
@@ -59,10 +57,10 @@ RSpec.describe Admins::PrizesController, type: :controller do
       expect(updated_prize.cost).to eq new_cost
     end
 
-    it "redirects to the admin dashboard" do
+    it 'redirects to the admin dashboard' do
       sign_in create(:admin)
       new_prize = create(:prize)
-      put :update, params: {id: prize.id, prize: {title: new_prize.title, quantity: new_prize.quantity, cost: new_prize.cost}}
+      put :update, params: { id: prize.id, prize: { title: new_prize.title, quantity: new_prize.quantity, cost: new_prize.cost } }
       expect(response).to redirect_to admins_dashboards_show_path
     end
   end
