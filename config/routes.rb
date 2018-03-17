a2b96437342c5c72d1488d346847864e07a86ec5
Joe <jdg006@lvc.edu>
2018-03-17 00:57:10 +0000

    config dashboards vs dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   namespace :admins do
     get 'dashboards/show'
+    resource :dashboard, only: [:show]
     resources 'prizes', only: [:new, :create, :edit, :update]
     resources 'stations', only: [:new, :create]
     resources 'admins', only: [:index, :new, :create, :destroy]
@@ -19,3 +20,4 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
