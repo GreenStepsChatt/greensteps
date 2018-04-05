9a6411be312a2767b82a7db2d53f2eeebab6fdbc
Joe <jdg006@lvc.edu>
2018-04-05 17:03:29 +0000

    DB schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -63,11 +63,10 @@ ActiveRecord::Schema.define(version: 20180316135831) do
     t.string "name"
     t.string "resource_type"
     t.integer "resource_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
     t.index ["name"], name: "index_roles_on_name"
-    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
   end
 
   create_table "stations", force: :cascade do |t|
@@ -106,9 +105,7 @@ ActiveRecord::Schema.define(version: 20180316135831) do
   create_table "users_roles", id: false, force: :cascade do |t|
     t.integer "user_id"
     t.integer "role_id"
-    t.index ["role_id"], name: "index_users_roles_on_role_id"
     t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
-    t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
   add_foreign_key "deeds", "users"
