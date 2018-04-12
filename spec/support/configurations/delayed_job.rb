c604aa7a25abf682b2e65415e4309adf57a139a1
Amanda Dolan <amandacrawfordalc@gmail.com>
2018-04-12 14:45:16 -0600

    DJ should immediately execute jobs only when needed

--- spec/support/configurations/delayed_job.rb
+++ spec/support/configurations/delayed_job.rb
@@ -1,1 +1,6 @@
-Delayed::Worker.delay_jobs = false
+RSpec.configure do |config|
+  config.around(:example, :immediately_execute_jobs) do |example|
+    Swapper.while(Delayed::Worker).
+      has(:delay_jobs).set_to(false) { example.run }
+  end
+end
