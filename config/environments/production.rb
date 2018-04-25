a8c9b268a71448594fcf97341f153722ec6e5325
Amanda Dolan <amandacrawfordalc@gmail.com>
2018-04-25 11:01:27 -0400

    Maybe we can temporarily set it to an empty string

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {
     host: ENV.fetch(
       "APPLICATION_HOST",
-      "#{ENV.fetch("HEROKU_APP_NAME")}.herokuapp.com"
+      "#{ENV.fetch("HEROKU_APP_NAME", '')}.herokuapp.com"
     )
   }
 
