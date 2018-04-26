4d011f1aefd63343dd74849e6a3290c6efe14bbd
Pierce McEntagart <pierce.mt92@gmail.com>
2018-04-26 15:18:51 -0400

    Remove unused method.

--- spec/page_objects/dashboard.rb
+++ spec/page_objects/dashboard.rb
@@ -4,10 +4,6 @@ module PageObjects
       this.find('.stats .add').click
     end
 
-    def trash_bag_count
-      this.find('.trash_bags .count').text.to_i
-    end
-
     def total_points
       this.find('.total_points .count').text.to_i
     end
