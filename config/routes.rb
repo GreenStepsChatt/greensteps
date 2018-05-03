bc7fd9164ababaf46ac462743b8cd590bd275498
Amanda Dolan <amandacrawfordalc@gmail.com>
2018-05-03 17:01:23 -0400

    Remove user_root_path so that admins are taken to their dashboard after log in

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,5 @@ Rails.application.routes.draw do
     end
   end
 
-  get 'dashboard', to: 'dashboards#show', as: :user_root
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
