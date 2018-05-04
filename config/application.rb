8c5964609109a1a0aa30485301873cba572d5165
Amanda Dolan <amandacrawfordalc@gmail.com>
2018-05-04 15:06:54 -0400

    Upgrade to Rails 5.2

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,8 @@ module Greensteps
     config.log_level = ENV.fetch('RAILS_LOG_LEVEL', 'debug')
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180425165953) do
+ActiveRecord::Schema.define(version: 2018_04_25_165953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
