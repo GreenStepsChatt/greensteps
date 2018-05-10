ee50b06c640b8b00b2b5a7ba4528ee9aa0295566
Amanda Dolan <amandacrawfordalc@gmail.com>
2018-05-10 15:18:59 -0400

    Implement direct upload to AWS
    Before, the image was uploaded to our Heroku server and then to S3
    storage. Now the image goes strait to S3.

--- spec/views/deeds/_form.html.erb_spec.rb
+++ spec/views/deeds/_form.html.erb_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe 'deeds/_form' do
   before(:each) { render 'deeds/form', deed: Deed.new }
 
   it { should validate_field('#deed_trash_bags').with(min: 0, step: 1) }
+  it { should have_direct_upload_field('#deed_before_photo') }
+  it { should have_direct_upload_field('#deed_after_photo') }
 end
