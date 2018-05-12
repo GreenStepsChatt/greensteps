a71c7048e79f571680d32f5b0e819bbdded473d1
Joe <jdg006@lvc.edu>
2018-05-12 09:09:43 +0000

    rubocop changes and specs added

--- app/controllers/admins/stations_controller.rb
+++ app/controllers/admins/stations_controller.rb
@@ -2,7 +2,7 @@ module Admins
   class StationsController < BaseController
     expose :station_form
     expose :station
-    
+
     def new; end
 
     def create
@@ -12,13 +12,12 @@ module Admins
         render 'new'
       end
     end
-    
+
     def update
-      
       station.assign_attributes(update_station_params)
       situation = determine_situation(station)
-      
-      if situation == "edit address"
+
+      if situation == 'edit address'
         station.address.assign_attributes(update_address_params)
         if station.save && station.address.save
           redirect_to admins_dashboard_path, notice: t('.success')
@@ -26,8 +25,7 @@ module Admins
           flash[:notice] = t('.alert')
           render :edit
         end
-      elsif situation == "delete address, create coordinate_pair"
-        flash[:notice] = 'This action will delete the address'
+      elsif situation == 'delete address, create coordinate_pair'
         station.address.delete
         station.coordinate_pair = create_coordinate_pair
         if station.coordinate_pair.save && station.save
@@ -36,7 +34,7 @@ module Admins
           flash[:notice] = t('.alert')
           render :edit
         end
-      elsif situation == "edit coordinate_pair"
+      elsif situation == 'edit coordinate_pair'
         station.coordinate_pair.assign_attributes(update_coordinate_pair_params)
         if station.save && station.coordinate_pair.save
           redirect_to admins_dashboard_path, notice: t('.success')
@@ -44,8 +42,7 @@ module Admins
           flash[:notice] = t('.alert')
           render :edit
         end
-      elsif situation == "delete coordinate_pair, create address"
-      flash.now[:alert] = 'This action will delete the coordinate pair'
+      elsif situation == 'delete coordinate_pair, create address'
         station.coordinate_pair.delete
         station.address = create_address
         if station.address.save && station.save
@@ -56,47 +53,47 @@ module Admins
         end
       else
         flash[:notice] = t('.alert')
-          render :edit
+        render :edit
       end
     end
 
     private
-    
+
     def determine_situation(station)
-      if station.address != nil && params[:longitude] == "" && params[:latitude] == ""
-        return "edit address"
-      elsif station.address != nil && params[:longitude] != "" && params[:latitude] != ""
-        return "delete address, create coordinate_pair"
-      elsif station.address == nil && params[:street] == "" && params[:city] == "" && params[:state] == "" && params[:zip] == ""
-        return "edit coordinate_pair"
-      elsif station.address == nil && params[:street] != "" && params[:city] != "" && params[:state] != "" && params[:zip] != ""
-        return "delete coordinate_pair, create address"
+      if !station.address.nil? && params[:longitude] == '' && params[:latitude] == ''
+        'edit address'
+      elsif !station.address.nil? && params[:longitude] != '' && params[:latitude] != ''
+        'delete address, create coordinate_pair'
+      elsif station.address.nil? && params[:street] == '' && params[:city] == '' && params[:state] == '' && params[:zip] == ''
+        'edit coordinate_pair'
+      elsif station.address.nil? && params[:street] != '' && params[:city] != '' && params[:state] != '' && params[:zip] != ''
+        'delete coordinate_pair, create address'
       else
-        return "incomplete information"
+        'incomplete information'
       end
     end
-    
+
     def update_station_params
       params.require(:station).permit(:name)
     end
-    
+
     def update_coordinate_pair_params
-        params.require(:coordinate_pair).permit(:longitude, :latitude)
+      params.require(:coordinate_pair).permit(:longitude, :latitude)
     end
-    
+
     def update_address_params
-        params.require(:address).permit(:street, :city, :state, :zip)
+      params.require(:address).permit(:street, :city, :state, :zip)
     end
 
     def station_form_params
       params.require(:station_form).permit(:name, :street, :city, :state, :zip,
                                            :latitude, :longitude)
     end
-    
+
     def create_address
       Address.create(street: params[:street], city: params[:city], state: params[:state], zip: params[:zip])
     end
-    
+
     def create_coordinate_pair
       CoordinatePair.create(longitude: params[:longitude], latitude: params[:latitude])
     end
--- spec/features/admins/stations_spec.rb
+++ spec/features/admins/stations_spec.rb
@@ -45,4 +45,138 @@ RSpec.describe 'Stations management', type: :feature do
     station_form.toggle_addr_latlon
     expect(page).to have_content 'Latitude'
   end
+
+  scenario 'Admin edits a station name', :js do
+    create_and_login_admin
+    station = create :station
+    address = create :address
+    station.address = address
+
+    visit edit_admins_station_path(station)
+
+    expect(find_field('Name').value).to eq station.name
+
+    fill_in 'Name', with: 'New name'
+
+    click_button 'Save'
+
+    expect(station_list).to include 'New name'
+  end
+
+  scenario 'Admin edits a station address', :js do
+    create_and_login_admin
+    station = create :station
+    address = create :address
+    station.address = address
+
+    visit edit_admins_station_path(station)
+
+    expect(find_field('address_street').value).to eq address.street
+    expect(find_field('address_city').value).to eq address.city
+    expect(find_field('address_state').value).to eq address.state
+    expect(find_field('address_zip').value).to eq address.zip
+
+    fill_in 'address_street', with: 'New Street'
+    fill_in 'address_city', with: 'New City'
+    select 'Pennsylvania', from: 'address_state'
+    fill_in 'address_zip', with: '00000'
+
+    click_button 'Save'
+
+    visit edit_admins_station_path(station)
+
+    expect(find_field('address_street').value).to eq 'New Street'
+    expect(find_field('address_city').value).to eq 'New City'
+    expect(find_field('address_state').value).to eq 'PA'
+    expect(find_field('address_zip').value).to eq '00000'
+  end
+
+  scenario 'Admin edits a station coordinate_pair', :js do
+    create_and_login_admin
+    station = create :station
+    coordinate_pair = create :coordinate_pair
+    station.coordinate_pair = coordinate_pair
+
+    visit edit_admins_station_path(station)
+
+    click_button 'use_coordinates'
+
+    expect(find_field('coordinate_pair_longitude').value).to eq coordinate_pair.longitude.to_s
+    expect(find_field('coordinate_pair_latitude').value).to eq coordinate_pair.latitude.to_s
+
+    fill_in 'coordinate_pair_longitude', with: 11
+    fill_in 'coordinate_pair_latitude', with: 11
+
+    click_button 'Save'
+
+    visit edit_admins_station_path(station)
+
+    click_button 'use_coordinates'
+
+    expect(find_field('coordinate_pair_longitude').value).to eq '11.0'
+    expect(find_field('coordinate_pair_latitude').value).to eq '11.0'
+  end
+
+  scenario 'Admin adds an address to a station with a coordinate pair which deletes the coordinate pair', :js do
+    create_and_login_admin
+    station = create :station
+    coordinate_pair = create :coordinate_pair
+    station.coordinate_pair = coordinate_pair
+    visit edit_admins_station_path(station)
+
+    click_button 'use_coordinates'
+
+    expect(find_field('coordinate_pair_longitude').value).to eq coordinate_pair.longitude.to_s
+    expect(find_field('coordinate_pair_latitude').value).to eq coordinate_pair.latitude.to_s
+
+    click_button 'use_address'
+
+    fill_in 'street', with: 'New Street'
+    fill_in 'city', with: 'New City'
+    select 'Pennsylvania', from: 'state'
+    fill_in 'zip', with: '00000'
+    click_button 'Save'
+    visit edit_admins_station_path(station)
+
+    expect(find_field('address_street').value).to eq 'New Street'
+    expect(find_field('address_city').value).to eq 'New City'
+    expect(find_field('address_state').value).to eq 'PA'
+    expect(find_field('address_zip').value).to eq '00000'
+
+    click_button 'use_coordinates'
+
+    expect(find_field('longitude').value).to eq ''
+    expect(find_field('latitude').value).to eq ''
+  end
+
+  scenario 'Admin adds a coordinate pair to a station with an address which deletes the address', :js do
+    create_and_login_admin
+    station = create :station
+    address = create :address
+    station.address = address
+    visit edit_admins_station_path(station)
+
+    expect(find_field('address_street').value).to eq address.street
+    expect(find_field('address_city').value).to eq address.city
+    expect(find_field('address_state').value).to eq address.state
+    expect(find_field('address_zip').value).to eq address.zip
+
+    click_button 'use_coordinates'
+
+    fill_in 'longitude', with: 11
+    fill_in 'latitude', with: 11
+
+    click_button 'Save'
+    visit edit_admins_station_path(station)
+
+    expect(find_field('street').value).to eq ''
+    expect(find_field('city').value).to eq ''
+    expect(find_field('state').value).to eq ''
+    expect(find_field('zip').value).to eq ''
+
+    click_button 'use_coordinates'
+
+    expect(find_field('coordinate_pair_longitude').value).to eq '11.0'
+    expect(find_field('coordinate_pair_latitude').value).to eq '11.0'
+  end
 end
