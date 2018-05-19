c887b7fe331748647e9b298c88d2918de6bb91be
Amanda Dolan <amandacrawfordalc@gmail.com>
2018-05-19 11:07:28 -0400

    Use deed factory when creating a deed

--- spec/factories/redemptions.rb
+++ spec/factories/redemptions.rb
@@ -4,16 +4,16 @@ FactoryBot.define do
     association :prize, factory: :prize, cost: 1
 
     transient do
-      invalid false
+      force_invalid false
     end
 
     trait :invalid do
-      invalid true
+      force_invalid true
     end
 
     after(:build) do |redemption, evaluator|
-      next if redemption.valid? || evaluator.invalid
-      redemption.user.deeds.create(trash_bags: 1)
+      next if redemption.valid? || evaluator.force_invalid
+      create :deed, user: redemption.user, trash_bags: 1
     end
   end
 end
