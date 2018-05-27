0fb012e34b2df9243818a5c3db5dcea44e2be4bc
Amanda Dolan <amandacrawfordalc@gmail.com>
2018-05-27 07:53:33 -0400

    Admins should see a link to gift card requests on their dashboard

--- spec/views/admins/dashboards/show.html.erb_spec.rb
+++ spec/views/admins/dashboards/show.html.erb_spec.rb
@@ -6,4 +6,5 @@ RSpec.describe 'admins/dashboards/show' do
   end
 
   it { should have_link t('admins.dashboards.show.miscellaneous.user_list') }
+  it { should have_link t('admins.dashboards.show.miscellaneous.gift_card_requests') }
 end
