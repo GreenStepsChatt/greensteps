e3f22bd63cd7d1642011677bb131cbab90b7df84
Amanda Dolan <amandacrawfordalc@gmail.com>
2018-05-27 07:44:33 -0400

    Test that the stats section gets updated

--- spec/features/redemptions_spec.rb
+++ spec/features/redemptions_spec.rb
@@ -9,5 +9,6 @@ RSpec.describe 'Redemptions', type: :feature do
 
     expect(page).to flash_message t('redemptions.create.success')
     expect(dashboard).to_not have_gift_card_button
+    expect(dashboard.points_redeemed_this_month).to eq 5
   end
 end
